0caca822f094818954a3403770dc3d65bc93c382
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2024-04-04 13:30:51 +0000

    finishing saleorderheader and its infered tables

--- dbt_wholesaleDW/models/NDS/Sales/sales_CreditCard.sql
+++ dbt_wholesaleDW/models/NDS/Sales/sales_CreditCard.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
 /*
     Basically, the creditcard now is the union between that in Wholesaling and Ecomerce (Simple Assumption). 
@@ -6,24 +6,28 @@
 
 with CTE as (
     select
+        creditcardid as old_creditcardid,
         cardnumber,
         cardtype,
         expmonth,
         expyear,
         modifieddate,
         is_deleted,
-        date_partition
+        date_partition,
+        "ecom" as source
     from {{ source('ecomerce', 'ecomerce_usercreditcard') }}
     union all
-    select
+    select 
+        creditcardid as old_creditcardid,
         cardnumber,
         cardtype,
         expmonth,
         expyear,
         modifieddate,
         is_deleted,
-        date_partition
-    from {{ source('wholesale', 'wholesale_system_storerepcreditcard') }}
+        date_partition,
+        "wholesale" as source
+    from {{ source('wholesale', 'wholesale_system_storerepcreditcard')}}
 )
 select 
     row_number() over(order by CTE.cardnumber, CTE.cardtype, CTE.expmonth, CTE.expyear) as creditcardid,
