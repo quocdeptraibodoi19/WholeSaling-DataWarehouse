1dfbfc02addceb67e4118055e3b4008e6a785ad2
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2024-04-11 18:44:40 +0000

    port to use spark

--- local-env-example.sh
+++ local-env-example.sh
@@ -27,28 +27,14 @@ export ecom_database=Ecomerce
 export ecom_user=
 export ecom_pass=
 
-export hadoop_host=hadoop-master
+export hadoop_host=node01
 export hadoop_port=9870
-export hadoop_user=hadoop
+export hadoop_user=hdfs
 
 export hdfs_host=$hadoop_host
 export hdfs_port=9001
 
 export hive_host=$hadoop_host
 export hive_port=10000
-export hive_user=$hadoop_user
+export hive_user=hive
 export hive_database=staging
-
-export presto_host=$hadoop_host
-export presto_port=8080
-export presto_user=$hadoop_user
-export presto_catalog=hive
-export presto_schema=staging
-
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
-export HADOOP_CONF_DIR=$AIRFLOW_HOME/config/
-export HADOOP_USER_NAME=hadoop
-
-export spark_master=yarn
-export spark_hive_metastore_uris=thrift://$hadoop_host:9083
-export spark_sql_warehouse_dir=hdfs://$hadoop_host:9001/sparksql-dw/
