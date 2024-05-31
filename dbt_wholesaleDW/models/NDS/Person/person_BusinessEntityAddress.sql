adf440045739e34d83ed4a525e2958498b576d20
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2024-06-01 03:25:21 +0700

    bussinessentityaddress

--- dbt_wholesaleDW/models/NDS/Person/person_BusinessEntityAddress.sql
+++ dbt_wholesaleDW/models/NDS/Person/person_BusinessEntityAddress.sql
@@ -1,10 +1,13 @@
 {{ config(materialized='view') }}
 
 select
-    s.bussinessentityid,
-    t.addressid,
-    t.addresstypeid,
-    t.modifieddate
+    s.bussiness_entity_id,
+    t.addressid as address_id,
+    t.address_type_id,
+    t.valid_from,
+    t.valid_to,
+    t.is_deleted,
+    t.is_valid
 from {{ ref("person_BussinessEntity") }} as s
 inner join {{ ref("person_Address") }} t
 on s.external_id = t.source_key and s.source = t.source
