a37833356e7196fe81363ddb7bffe9c76b9935c0
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2024-06-13 22:19:46 +0700

    allow CORSMiddleware for frontend

--- visualization/backend/main.py
+++ visualization/backend/main.py
@@ -7,6 +7,19 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 from src.router.charts import router as chart_router
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router=chart_router)
+
+allowed_origins = [
+    "http://0.0.0.0:8084",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
